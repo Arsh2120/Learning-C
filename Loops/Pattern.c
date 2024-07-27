6c3b6f626062d65f37e6a9cd85b584056699ffe9
Arsh2120 <arshdhiman458@gmail.com>
2024-07-27 22:29:18 +0530

    parttern

---
+++ Loops/Pattern.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,k;
+    printf("Enter the number of rows\n");
+    scanf("%d", &n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+        for(k=1;k<=2*i-1;k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=n-1;i>=1;i--)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+        for(k=1;k<=2*i-1;k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
