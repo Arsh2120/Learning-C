a899e593911a5c493b8fc294479eb3bb20dfccae
Arsh2120 <arshdhiman458@gmail.com>
2024-08-02 13:40:37 +0530

    Sum of elements in array

--- 4.Arrays/1.Sum.c
+++ 4.Arrays/1.Sum.c
@@ -2,15 +2,15 @@
 #include<stdio.h>
 int main()
 {
-    int arr[50],i=0,n=0,sum=0;
+    int arr[50],i=0,n=0,s=0;
     printf("Enter number of elements\n");
     scanf("%d", &n);
     printf("Enter the elements\n");
     for(i=0;i<n;i++)
     {
         scanf("%d", &arr[i]);
-        sum+=arr[i];
+        s+=arr[i];
     }
-    printf("Sum is %d",sum);
+    printf("Sum: %d",s);
     return 0;
 }
